58ff6df2044c0442a2dadc33127eae50429f20ee
Karin Abe <karin.abe@gmail.com>
2023-09-06 01:36:46 +0000

    rota em html da pagina principal

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask("meu app")
 
-@app.route('/')
-def hello():
- return "Hello World"
+posts = [
+    {
+    "titulo": "Minha primeira postagem",
+    "texto": "teste"
+    },
+    {
+    "titulo": "Segundo Post",
+    "texto": "outro teste"
+    }
+]
 
-@app.route("/novo")
-def novo():
- return "novo"
- 
+@app.route('/')
+def exibir_entrada():
+    entradas = posts # Mock das postagens
+    return render_template('exibir_entradas.html', entradas=entradas) 
